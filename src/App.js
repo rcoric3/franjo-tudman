48b6aa2683f34d347ebac70c560eb6416b5de49f
Romeo Coric <Romeo.Coric@netcetera.com>
2024-05-29 10:54:43 +0200

    responsive

--- src/App.js
+++ src/App.js
@@ -49,6 +49,13 @@ const App = () => {
     speechSynthesis.speak(utterance);
   };
 
+  const videoOptions = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
   return (
     <Box
       sx={{
@@ -58,6 +65,16 @@ const App = () => {
         height: "100vh",
         overflow: "hidden",
         color: "#fff",
+        padding: "20px",
+        "@media (max-width: 768px)": {
+          // Styles für iPad und kleinere Geräte
+          marginTop: "20px",
+        },
+        "@media (max-width: 480px)": {
+          // Styles für iPhone und kleinere Geräte
+          marginTop: "10px",
+          padding: "10px",
+        },
       }}
     >
       <Box
@@ -74,7 +91,21 @@ const App = () => {
           zIndex: -1,
         }}
       />
-      <h1>Pritisni Franja Tuđmana </h1>
+      <h1
+        style={{
+          fontSize: "2em",
+          "@media (max-width: 768px)": {
+            // Styles für iPad und kleinere Geräte
+            fontSize: "1.5em",
+          },
+          "@media (max-width: 480px)": {
+            // Styles für iPhone und kleinere Geräte
+            fontSize: "1.2em",
+          },
+        }}
+      >
+        Pritisni Franja Tuđmana
+      </h1>
       <Box
         component="img"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Franjo_Tudjman_1995.jpg/640px-Franjo_Tudjman_1995.jpg"
@@ -87,6 +118,14 @@ const App = () => {
           display: "block",
           marginLeft: "auto",
           marginRight: "auto",
+          "@media (max-width: 768px)": {
+            // Styles für iPad und kleinere Geräte
+            width: "200px",
+          },
+          "@media (max-width: 480px)": {
+            // Styles für iPhone und kleinere Geräte
+            width: "150px",
+          },
         }}
         onClick={handleClick}
       />
@@ -98,12 +137,38 @@ const App = () => {
           backgroundColor: "#fff",
           color: "#000",
           fontWeight: "bold",
+          "@media (max-width: 768px)": {
+            // Styles für iPad und kleinere Geräte
+            padding: "8px 16px",
+            fontSize: "0.9em",
+          },
+          "@media (max-width: 480px)": {
+            // Styles für iPhone und kleinere Geräte
+            padding: "6px 12px",
+            fontSize: "0.8em",
+          },
         }}
       >
         {playMusic ? "Stop Music" : "Play Music"}
       </Button>
       {playMusic && (
-        <YouTube videoId="au-DC7l8Hb8" opts={{ playerVars: { autoplay: 1 } }} />
+        <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+            "@media (max-width: 768px)": {
+              // Styles für iPad und kleinere Geräte
+              width: "90%",
+            },
+            "@media (max-width: 480px)": {
+              // Styles für iPhone und kleinere Geräte
+              width: "80%",
+            },
+          }}
+        >
+          <YouTube videoId="au-DC7l8Hb8" opts={videoOptions} />
+        </Box>
       )}
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>Jebo Mamu</DialogTitle>
