66374d84f936dc502c7bb85c22c48e209e1f79f4
Romeo Coric <Romeo.Coric@netcetera.com>
2024-05-31 16:54:30 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import React, { useState, useRef, useEffect } from "react";
-import {
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-} from "@mui/material";
-import { Box } from "@mui/system";
+import React from "react";
 import Croatia from "./Croatia";
 import Albania from "./Albania";
-import './css/global.css'
+import { Route, wouter } from "wouter";
+import "./css/global.css";
 
 const App = () => {
   return (
     <>
-     <Croatia />
-      <Albania />
+      <Route path={"/croatia"} component={Croatia} />
+      <Route path={"/albania"} component={Albania} />
     </>
   );
 };
