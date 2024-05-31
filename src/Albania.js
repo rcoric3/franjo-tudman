e65ed174e6fc339839dba55796e730e7ae59f93c
taabyer1 <erson.bytyqi@swisscom.com>
2024-05-31 14:26:25 +0200

    shqiperia

---
+++ src/Albania.js
@@ -0,0 +1,107 @@
+import React, { useState, useRef, useEffect } from "react";
+import "./css/alb.css";
+import Adem from './img/adem.jpg';
+import Gabelic from './audios/gabel.wav';
+import OtherAudioFile from './audios/marsh.mp3';
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
+import { Box } from "@mui/system";
+
+function Albania() {
+  const [open, setOpen] = useState(false);
+  const [popupText, setPopupText] = useState("");
+  const [sentenceIndex, setSentenceIndex] = useState(0);
+  const [voices, setVoices] = useState([]);
+  const otherAudioRef = useRef(null);
+  const gabelAudioRef = useRef(null);
+  const audioRef = useRef(null);
+
+  const sentences = [
+    "Shqipëria do të jetë një shtet i pavarur.",
+    "Ne nuk mund të lejojmë që Serbia të dominojë mbi ne.",
+    "Populli shqiptar do t'i rezistojë agresionit.",
+    "Shqipëria do të mbrohet me të gjitha mjetet."
+  ];
+
+  const handleAdemClick = () => {
+    const nextSentence = sentences[sentenceIndex];
+    setPopupText(nextSentence);
+    setOpen(true);
+    speakText(nextSentence);
+    setSentenceIndex((prevIndex) => (prevIndex + 1) % sentences.length);
+    if (audioRef.current) {
+      audioRef.current.play();
+    }
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOtherButtonClick = () => {
+    if (otherAudioRef.current) {
+      otherAudioRef.current.play();
+    }
+  };
+
+  const handleGabelAudioClick = () => {
+    if (gabelAudioRef.current) {
+      gabelAudioRef.current.play();
+    }
+  };
+
+  // Funktion für die Sprachausgabe
+  const speakText = (text) => {
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.lang = "sq-AL"; // Setze die Sprache auf Albanisch
+    utterance.voice = voices.find((voice) => voice.lang === "sq-AL");
+    speechSynthesis.speak(utterance);
+  };
+
+  useEffect(() => {
+    // Stimmen laden, da getVoices() asynchron sein kann
+    if (typeof window !== "undefined" && window.speechSynthesis) {
+      const loadVoices = () => {
+        const availableVoices = window.speechSynthesis.getVoices();
+        setVoices(availableVoices);
+      };
+      window.speechSynthesis.onvoiceschanged = loadVoices;
+      loadVoices();
+    }
+  }, []);
+
+  return (
+    <div className="albania-container">
+      <button className="adem-button" onClick={handleAdemClick}>
+        <h1>Bac u kry!</h1>
+        <img src={Adem} alt="Adem Button" />
+      </button>
+      <div>
+        <button className="other-button" onClick={handleOtherButtonClick}>
+          Marshi
+        </button>
+      </div>
+      <div>
+        <button className="other-button" onClick={handleGabelAudioClick}>
+          Gabelic
+        </button>
+      </div>
+      <audio ref={otherAudioRef} src={OtherAudioFile} />
+      <audio ref={gabelAudioRef} src={Gabelic} />
+      <audio ref={audioRef} src={OtherAudioFile} loop />
+
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Informacion</DialogTitle>
+        <DialogContent>
+          <DialogContentText>{popupText}</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
+
+export default Albania;
---
+++ src/Croatia.js
@@ -0,0 +1,230 @@
+import React, { useState, useRef, useEffect } from "react";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
+import { Box } from "@mui/system";
+
+function Croatia() {
+    const [open, setOpen] = useState(false);
+    const [popupText, setPopupText] = useState("");
+    const [sentenceIndex, setSentenceIndex] = useState(0);
+    const [playMusic, setPlayMusic] = useState(false);
+    const [count, setCount] = useState(0);
+    const audioRef = useRef(null);
+    const audioRef2 = useRef(null);
+    const audioRef3 = useRef(null);
+  
+    const sentences = [
+      "Hrvatska će biti nezavisna država.",
+      "Srbi će biti manjina u Hrvatskoj.",
+      "Ne možemo dopustiti da Srbija dominira nad nama.",
+      "Hrvatski narod će se oduprijeti agresiji.",
+      "Hrvatska će se braniti svim sredstvima.",
+    ];
+  
+    const handleClick = () => {
+      const nextSentence = sentences[sentenceIndex];
+      setPopupText(nextSentence);
+      setOpen(true);
+      speakText(nextSentence);
+      setSentenceIndex((prevIndex) => (prevIndex + 1) % sentences.length);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
+    const handleMusicToggle = () => {
+      setPlayMusic(!playMusic);
+      if (!playMusic) {
+        audioRef.current.play();
+      } else {
+        audioRef.current.pause();
+      }
+    };
+  
+    const handleCountAndPlaySound = () => {
+      setCount(count + 1);
+      audioRef2.current.play();
+    };
+  
+    const handleGreetPlaySound = () => {
+      audioRef3.current.play();
+    };
+  
+    useEffect(() => {
+      if (audioRef.current) {
+        audioRef.current.volume = 0.2;
+      }
+    }, []);
+  
+    // Funktion für die Sprachausgabe
+    const speakText = (text) => {
+      const utterance = new SpeechSynthesisUtterance(text);
+      utterance.lang = "hr-HR"; // Setze die Sprache auf Kroatisch
+      utterance.voice = speechSynthesis
+        .getVoices()
+        .find((voice) => voice.lang === "hr-HR");
+      speechSynthesis.speak(utterance);
+    };
+  
+  return (
+    <Box
+    sx={{
+      position: "relative",
+      textAlign: "center",
+      marginTop: "50px",
+      height: "100vh",
+      overflow: "hidden",
+      color: "#fff",
+      padding: "20px",
+      "@media (max-width: 768px)": {
+        marginTop: "20px",
+      },
+      "@media (max-width: 480px)": {
+        marginTop: "10px",
+        padding: "10px",
+      },
+    }}
+  >
+    <Box
+      component="div"
+      sx={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        backgroundImage: `url(https://upload.wikimedia.org/wikipedia/commons/1/1b/Flag_of_Croatia.svg)`,
+        backgroundSize: "cover",
+        filter: "blur(8px)",
+        zIndex: -1,
+      }}
+    />
+    <h1
+      style={{
+        fontSize: "2em",
+        "@media (max-width: 768px)": {
+          fontSize: "1.5em",
+        },
+        "@media (max-width: 480px)": {
+          fontSize: "1.2em",
+        },
+      }}
+    >
+      Pritisni Franja Tuđmana
+    </h1>
+    <Box
+      component="img"
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Franjo_Tudjman_1995.jpg/640px-Franjo_Tudjman_1995.jpg"
+      alt="Franjo Tudjman"
+      sx={{
+        cursor: "pointer",
+        width: "300px",
+        border: "2px solid #ccc",
+        borderRadius: "10px",
+        display: "block",
+        marginLeft: "auto",
+        marginRight: "auto",
+        "@media (max-width: 768px)": {
+          width: "200px",
+        },
+        "@media (max-width: 480px)": {
+          width: "150px",
+        },
+      }}
+      onClick={handleClick}
+    />
+    <Button
+      onClick={handleMusicToggle}
+      sx={{
+        marginTop: "20px",
+        padding: "10px 20px",
+        backgroundColor: "#fff",
+        color: "#000",
+        fontWeight: "bold",
+        "@media (max-width: 768px)": {
+          padding: "8px 16px",
+          fontSize: "0.9em",
+        },
+        "@media (max-width: 480px)": {
+          padding: "6px 12px",
+          fontSize: "0.8em",
+        },
+      }}
+    >
+      {playMusic ? "Stop Music" : "Play Music"}
+    </Button>
+    <audio ref={audioRef} src="/sound.mp3" loop />
+    <audio ref={audioRef2} src="/sound2.mp3" />
+    <audio ref={audioRef3} src="/sound3.mp3" />
+    <Box
+      sx={{
+        marginTop: "20px",
+      }}
+    >
+      <Button
+        onClick={handleCountAndPlaySound}
+        sx={{
+          padding: "10px 20px",
+          backgroundColor: "#fff",
+          color: "#000",
+          fontWeight: "bold",
+          "@media (max-width: 768px)": {
+            padding: "8px 16px",
+            fontSize: "0.9em",
+          },
+          "@media (max-width: 480px)": {
+            padding: "6px 12px",
+            fontSize: "0.8em",
+          },
+        }}
+      >
+        NWord Counter: {count}
+      </Button>
+    </Box>
+    <Box
+      sx={{
+        marginTop: "20px",
+      }}
+    >
+      <Button
+        onClick={handleGreetPlaySound}
+        sx={{
+          padding: "10px 20px",
+          backgroundColor: "#fff",
+          color: "#000",
+          fontWeight: "bold",
+          "@media (max-width: 768px)": {
+            padding: "8px 16px",
+            fontSize: "0.9em",
+          },
+          "@media (max-width: 480px)": {
+            padding: "6px 12px",
+            fontSize: "0.8em",
+          },
+        }}
+      >
+        Greetings from Fuerer
+      </Button>
+    </Box>
+    <Dialog open={open} onClose={handleClose}>
+      <DialogTitle>Mamu ti jebem</DialogTitle>
+      <DialogContent>
+        <DialogContentText>{popupText}</DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} color="primary">
+          Close
+        </Button>
+      </DialogActions>
+    </Dialog>
+  </Box>
+  )
+}
+
+export default Croatia
