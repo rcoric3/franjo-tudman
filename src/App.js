9005e9bdbbf5f032f48a24bdb05ddbe94bee106f
Dior <diortalh@gmail.com>
2024-06-01 01:55:53 +0200

    ti qi ropt

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
+import Navbar from "./Navbar"; 
 import Croatia from "./Croatia";
 import Albania from "./Albania";
+import Madeume from "./Madeume";
 import { Route } from "wouter";
 import "./css/global.css";
 
 const App = () => {
   return (
     <>
+      <Navbar />
       <Route path={"/croatia"} component={Croatia} />
       <Route path={"/albania"} component={Albania} />
+      <Route path={"/"} component={Madeume} />
     </>
   );
 };
